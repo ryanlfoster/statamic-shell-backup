8d4bd859d23affc8de97758e702e8ae4c41ec018
Clinton Reeves <clinton@parcel8.com>
2014-02-28 15:34:33 -0700

    Rename the root path variable to something a bit more concise

--- backup_site.sh
+++ backup_site.sh
@@ -2,8 +2,8 @@
 
 #Set Directory Information....
 this_directory=$(dirname -- $(readlink -fn -- "$0"))
-webroot="$(dirname "$this_directory")"
-directory_to_backup="$webroot/web/content"
+root_path="$(dirname "$this_directory")"
+directory_to_backup="$root_path/web/content"
 send_to_cloud_path="$this_directory/php/config.php"
 
 #Set the backup file name
@@ -17,5 +17,5 @@ zip -r $this_directory/"backups"/$backup_name $directory_to_backup
 #First argument is the location of the backup file, second argument is the name to be used when uploaded
 php $send_to_cloud_path $this_directory/"backups"/$backup_name $backup_name
 
-#After your backup has been uploaded, remove the tar ball from the filesystem.
+#After your backup has been uploaded, remove the zip file from the filesystem.
 rm $this_directory/"backups"/$backup_name 
